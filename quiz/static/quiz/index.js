4dd0d6b6905b5a16bc76417884e83ce80bcc5f5a
Robert Moga <robertmoga16@gmail.com>
2018-04-21 17:43:57 +0300

    new pages and new buuton

--- quiz/static/quiz/index.js
+++ quiz/static/quiz/index.js
@@ -6,3 +6,18 @@ function toProfile()
 //    $.post("", { but_pressed : buttonPressed});
 }
 
+function revealFields(){
+    node = document.getElementById('hidden')
+    var x = document.createElement("INPUT");
+    x.setAttribute("type", "text");
+    x.setAttribute("id", "inputText");
+    node.appendChild(x);
+
+    var y = document.createElement("INPUT");
+    y.setAttribute("type", "submit");
+    y.setAttribute("id", "join_but");
+    y.setAttribute("value", "START NOW");
+    node.appendChild(y);
+
+    var text =
+}
