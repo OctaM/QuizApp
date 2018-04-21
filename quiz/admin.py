550dd544183f40c483323656cad4dc852ff2921d
Octavian <omarian@lampix.co>
2018-04-21 15:10:54 +0300

    create models on server

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import User, Question, Round, Answer, Language, Game
 # Register your models here.
+
+
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Round)
+admin.site.register(Answer)
+admin.site.register(Language)
+admin.site.register(Game)
---
+++ quiz/classes/user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, username, password, mother_tongue, target_tongues, leader_board_score):
+        self.username = username
+        self.password = password
+        self.mother_tongue = mother_tongue
+        self.target_tongues = target_tongues
+        self.leader_board_scores = leader_board_score
+
